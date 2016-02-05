cbaabe7def68916fbc3ba9c2eb7c28d5eba9a10d
William <wmconnor1986@gmail.com>
2016-02-05 08:30:49 -0500

    restyling the button

---
+++ resources/views/button.blade.php
@@ -0,0 +1,20 @@
+@extends('/app')
+
+@section('title')
+    Button
+@endsection
+
+@section('content')
+<div class="button-meta-data">
+    name of button
+    <div class="clicks" id="clicks">number of button clicks</div>
+</div>
+
+<div class="button-container">
+    <button id="button" class="circle-button"></button>
+</div>
+@endsection
+
+@section('foot-script')
+    <script src="/js/script.js"></script>
+@endsection
