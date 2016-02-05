f95f8380c4d6cd5fa39a9fb12b1aaafd8304253e
William <wmconnor1986@gmail.com>
2016-02-05 17:22:30 -0500

    button link vs button button

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,10 +8,12 @@
         </button>
     </div>
 
-    <div class="button-container">
-        <button id="find_button" class="index-button" style="background-color: {{ $find_color }}; color: {{ $find_text_color }}">
-            I Already Have A Button!
-        </button>
+    <div class="round-button">
+        <div class="round-button-circle" style="background-color: {{ $find_color }}">
+            <a href="http://jsfiddle.net/user/josedvq/fiddles/" class="round-button" style="color: {{ $find_text_color }}">
+                I Already Have A Button!
+            </a>
+        </div>
     </div>
 @endsection
 
