8c8bda85b75a58ba2fa3b86912617a9a7470fdbe
William <wmconnor1986@gmail.com>
2016-02-06 18:18:01 -0500

    added migration for button table

---
+++ database/migrations/2016_02_06_221308_create_buttons_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateButtonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buttons', function(Blueprint $table)
+        {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('password');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('buttons');
+    }
+}
